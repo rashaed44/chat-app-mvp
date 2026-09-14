efc08ceaadc85ce75de5f4ee20f6d708260507e8
rashaed44 <rm4609282@gmail.com>
2026-09-14 18:30:26 +0300

    update App.tsx

--- App.tsx
+++ App.tsx
@@ -1,11 +1,24 @@
 import React from 'react';
-import { NavigationContainer } from '@react-navigation/native';
-import MainTabs from './src/navigation/MainTabs';
+import { StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
   return (
-    <NavigationContainer>
-      <MainTabs />
-    </NavigationContainer>
+    <View style={styles.container}>
+      <Text style={styles.title}>مرحباً بك في تطبيق المحادثة!</Text>
+    </View>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  title: {
+    fontSize: 20,
+    fontWeight: 'bold',
+  },
+});
+
